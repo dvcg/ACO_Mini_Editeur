2542f6e4cec194e2c897385820f7f3c814c20473
dianevalet <dianevalet@yahoo.fr>
2017-11-15 20:06:33 +0200

    commit 2

---
+++ miniediteur.v1/receiver/MoteurEditeur_Impl.java
@@ -0,0 +1,102 @@
+/**
+ * 
+ */
+package receiver;
+
+/**
+ * @author DIANA
+ *
+ */
+public class MoteurEditeur_Impl implements MoteurEditeur {
+
+	/* (non-Javadoc)
+	 * @see receiver.MoteurEditeur#getBuffer()
+	 */
+	@Override
+	public Buffer getBuffer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see receiver.MoteurEditeur#getSelection()
+	 */
+	@Override
+	public Selection getSelection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see receiver.MoteurEditeur#getPressePapier()
+	 */
+	@Override
+	public PressePapier getPressePapier() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see receiver.MoteurEditeur#couper()
+	 */
+	@Override
+	public void couper() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see receiver.MoteurEditeur#copier()
+	 */
+	@Override
+	public void copier() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see receiver.MoteurEditeur#coller()
+	 */
+	@Override
+	public void coller() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see receiver.MoteurEditeur#insererTexte(java.lang.String)
+	 */
+	@Override
+	public void insererTexte(String texte) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see receiver.MoteurEditeur#selectionner(int, int)
+	 */
+	@Override
+	public void selectionner(int debut, int longueur) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see receiver.MoteurEditeur#retourArriere()
+	 */
+	@Override
+	public void retourArriere() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see receiver.MoteurEditeur#supprimer()
+	 */
+	@Override
+	public void supprimer() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
